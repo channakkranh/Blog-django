8063e94b12313a4090bef6c06a020d4f9b3baf3c
Channak Kranh <channak.kranh@vital.com.kh>
2023-04-05 17:29:37 +0700

    add admin site

--- acticles_app/admin.py
+++ acticles_app/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
 # Register your models here.
+from.models import Acticle
+admin.site.register(Acticle)
+
--- acticles_app/models.py
+++ acticles_app/models.py
@@ -6,3 +6,5 @@ class Acticle (models.Model):
     body=models.TextField()
     date=models.DateTimeField(auto_now_add=True)
 
+def __str__(self):
+    return self.title
